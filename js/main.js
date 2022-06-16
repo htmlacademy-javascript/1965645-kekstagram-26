02466017a632695c1c1533ad11b980621e563a50
Мария Бейлина <marie.beilina3030@gmail.com>
2022-06-16 10:28:15 +0300

    Две вспомогательные функции

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
 //Функция, возвращающая случайное целое число из переданного диапазона
-const getRandomNumber = (min, max) => {
+const getRandomNumberFromRange = (min, max) => {
   if ((min >= max) || (min < 0)) {
     throw new Error ('первое число должно быть меньше второго, оба числа должны быть положительными');
   }
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 };
-getRandomNumber (0, 45);
+getRandomNumberFromRange (0, 45);
 
 
 //Функция для проверки длины строки
-const checkLength = (string, maxlength) => {
-  if (string.length > maxlength) {
-    return false;
-  }
-  return true;
-};
-checkLength('aaaa', 3);
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+checkStringLength('aaaa', 3);
