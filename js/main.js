0aa07e7f24dcab5ab21bf26e578947a5bacc3e42
Мария Бейлина <marie.beilina3030@gmail.com>
2022-06-27 14:59:58 +0300

    Отрисовка

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-import {createPosts, SIMILAR_POST_COUNT} from './generate-data.js';
-import {renderPosts} from './render-data.js';
-renderPosts (createPosts(SIMILAR_POST_COUNT));
+import {createPosts, SIMILAR_POST_COUNT} from './generate-posts.js';
+import {renderPosts} from './render-posts.js';
+renderPosts(createPosts(SIMILAR_POST_COUNT));
