87e49793cabd07f681d38a888161c2c54c902679
Мария Бейлина <marie.beilina3030@gmail.com>
2022-07-20 10:55:08 +0300

    Редактирование изображения

--- js/new-post-form.js
+++ js/new-post-form.js
@@ -1,11 +1,116 @@
 import { isEscapeKey } from './util.js';
 const MAX_COMMENT_LENGTH = 140;
-const RE_HASHTAG = /(^#[A-Za-zА-Яа-яЁё0-9]{1,20}\b\s?)((\b\s#[A-Za-zА-Яа-яЁё0-9]{1,20}\b\s?){1,4})?$/;
+const RE_HASHTAG = /(^#[A-Za-zА-Яа-яЁё0-9]{1,19}\b\s?)((\b\s#[A-Za-zА-Яа-яЁё0-9]{1,19}\b\s?){1,4})?$/;
 
-const DEFAULT_VALUE = 100;
+const DEFAULT_SCALE_VALUE = 100;
 const SCALE_STEP = 25;
-const MAX_VALUE = 100;
-const MIN_VALUE = 25;
+const MAX_SCALE_VALUE = 100;
+const MIN_SCALE_VALUE = 25;
+
+const effectType = {
+  NONE: 'none',
+  CHROME: 'chrome',
+  SEPIA: 'sepia',
+  MARVIN: 'marvin',
+  PHOBOS: 'phobos',
+  HEAT: 'heat',
+};
+
+const effectValue = {
+  [effectType.CHROME]: 'grayscale',
+  [effectType.SEPIA]: 'sepia',
+  [effectType.MARVIN]: 'invert',
+  [effectType.PHOBOS]: 'blur',
+  [effectType.HEAT]: 'brightness'
+};
+
+const effectLevelConfig = {
+  [effectType.CHROME]: {
+    range: {
+      min: 0,
+      max: 1
+    },
+    start: 1,
+    step: 0.1,
+    connect: 'lower',
+    format: {
+      to: function (value) {
+        return (value);
+      },
+      from: function (value) {
+        return (value);
+      },
+    }
+  },
+  [effectType.SEPIA]: {
+    range: {
+      min: 0,
+      max: 1
+    },
+    start: 1,
+    step: 0.1,
+    connect: 'lower',
+    format: {
+      to: function (value) {
+        return (value);
+      },
+      from: function (value) {
+        return (value);
+      },
+    }
+  },
+  [effectType.MARVIN]: {
+    range: {
+      min: 0,
+      max: 100
+    },
+    start: 100,
+    step: 1,
+    connect: 'lower',
+    format: {
+      to: function (value) {
+        return (`${value}%`);
+      },
+      from: function (value) {
+        return (value);
+      },
+    }
+  },
+  [effectType.PHOBOS]: {
+    range: {
+      min: 0,
+      max: 3
+    },
+    start: 3,
+    step: 0.1,
+    connect: 'lower',
+    format: {
+      to: function (value) {
+        return (`${value}px`);
+      },
+      from: function (value) {
+        return (value);
+      },
+    }
+  },
+  [effectType.HEAT]: {
+    range: {
+      min: 1,
+      max: 3
+    },
+    start: 3,
+    step: 0.1,
+    connect: 'lower',
+    format: {
+      to: function (value) {
+        return (value);
+      },
+      from: function (value) {
+        return (value);
+      },
+    }
+  }
+};
 
 const uploadForm = document.querySelector('.img-upload__form');
 const editForm = uploadForm.querySelector('.img-upload__overlay');
@@ -14,16 +119,18 @@ const closeFormButton = uploadForm.querySelector('.img-upload__cancel');
 const postHashtag = uploadForm.querySelector('.text__hashtags');
 const postDescription = uploadForm.querySelector('.text__description');
 
-const smallerScaleButton = document.querySelector('.scale__control--smaller');
-const biggerScaleButton = document.querySelector('.scale__control--bigger');
-const scaleControlValue = document.querySelector('.scale__control--value');
-const imgPreview = document.querySelector('.img-upload__preview').querySelector('img');
+const smallerScaleButton = uploadForm.querySelector('.scale__control--smaller');
+const biggerScaleButton = uploadForm.querySelector('.scale__control--bigger');
+const scaleControlValue = uploadForm.querySelector('.scale__control--value');
+const imgPreview = uploadForm.querySelector('.img-upload__preview').querySelector('img');
 
-const effectRadioButtons = document.querySelectorAll('.effects__radio');
-const effectLevelSlider = document.querySelector('.effect-level__slider');
-const effectLevelValue = document.querySelector('.effect-level__value');
+const effectLevelSlider = uploadForm.querySelector('.effect-level__slider');
+const effectLevelValue = uploadForm.querySelector('.effect-level__value');
+const effectLevel = uploadForm.querySelector('.effect-level');
+const effectList = uploadForm.querySelector('.effects__list');
 
-let currentValue = DEFAULT_VALUE;
+let currentScaleValue = DEFAULT_SCALE_VALUE;
+let currentEffect;
 
 const pristine = new Pristine(uploadForm, {
   classTo: 'img-upload__field-wrapper',
@@ -45,34 +152,24 @@ pristine.addValidator(postHashtag, validateHashtag,'Хештег должен н
 pristine.addValidator(postHashtag, validateRepeatingHashtags, 'Хештеги не должны повторяться');
 pristine.addValidator(postDescription, validateComment, `Комментарий не должен быть длиннее ${MAX_COMMENT_LENGTH} символов`);
 
-
 const onSmallerScaleButtonClick = () => {
-  currentValue -= SCALE_STEP;
-  imgPreview.style.transform = `scale(${(currentValue) / 100})`;
-  scaleControlValue.value = `${currentValue} %`;
-
-  if (currentValue <= MIN_VALUE) {
-    currentValue = MIN_VALUE;
-    scaleControlValue.value = `${MIN_VALUE} %`;
-    imgPreview.style.transform = `scale(${(MIN_VALUE) / 100})`;
+  if (currentScaleValue === MIN_SCALE_VALUE) {
+    return;
   }
+
+  currentScaleValue -= SCALE_STEP;
+  imgPreview.style.transform = `scale(${(currentScaleValue) / 100})`;
+  scaleControlValue.value = `${currentScaleValue}%`;
 };
 
 const onBiggerScaleButtonClick = () => {
-  currentValue += SCALE_STEP;
-  imgPreview.style.transform = `scale(${(currentValue) / 100})`;
-  scaleControlValue.value = `${currentValue} %`;
-
-  if (currentValue >= MAX_VALUE) {
-    currentValue = MAX_VALUE;
-    scaleControlValue.value = `${MAX_VALUE} %`;
-    imgPreview.style.transform = `scale(${(MAX_VALUE) / 100})`;
+  if (currentScaleValue === MAX_SCALE_VALUE) {
+    return;
   }
-};
 
-const changeScale = () => {
-  smallerScaleButton.addEventListener('click', onSmallerScaleButtonClick);
-  biggerScaleButton.addEventListener('click', onBiggerScaleButtonClick);
+  currentScaleValue += SCALE_STEP;
+  imgPreview.style.transform = `scale(${(currentScaleValue) / 100})`;
+  scaleControlValue.value = `${currentScaleValue}%`;
 };
 
 noUiSlider.create(effectLevelSlider, {
@@ -85,111 +182,30 @@ noUiSlider.create(effectLevelSlider, {
   connect: 'lower',
 });
 
+const onEffectListChange = (evt) => {
+  if (evt.target.closest('.effects__radio')) {
+    currentEffect = evt.target.value;
+    imgPreview.className = `effects__preview--${currentEffect}`;
 
-const applyEffect = () => {
-  effectRadioButtons.forEach((effectRadioButton) => {
-
-    effectRadioButton.addEventListener ('change', () => {
-      imgPreview.className = `effects__preview--${effectRadioButton.value}`;
+    if (currentEffect !== effectType.NONE) {
+      effectLevelSlider.noUiSlider.reset();
       effectLevelValue.value = '';
+      effectLevelSlider.classList.remove ('hidden');
 
-      if (effectRadioButton.value === 'none') {
-        effectLevelSlider.classList.add('hidden');
-      } else {
-        effectLevelSlider.classList.remove('hidden');
-      }
-
-      if (effectRadioButton.value === 'chrome') {
-
-        effectLevelSlider.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 1
-          },
-          start: 1,
-          step: 0.1,
-          connect: 'lower',
-        });
-
-        effectLevelSlider.noUiSlider.on('update', () => {
-          effectLevelValue.value = effectLevelSlider.noUiSlider.get();
-          imgPreview.style.filter = `grayscale(${effectLevelValue.value})`;
-        });
-      }
-
-      if (effectRadioButton.value === 'sepia') {
-
-        effectLevelSlider.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 1
-          },
-          start: 1,
-          step: 0.1,
-          connect: 'lower',
-        });
-
-        effectLevelSlider.noUiSlider.on('update', () => {
-          effectLevelValue.value = effectLevelSlider.noUiSlider.get();
-          imgPreview.style.filter = `sepia(${effectLevelValue.value})`;
-        });
-      }
-
-      if (effectRadioButton.value === 'marvin') {
-        effectLevelSlider.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 100
-          },
-          start: 100,
-          step: 1,
-          connect: 'lower',
-        });
-        effectLevelSlider.noUiSlider.on('update', () => {
-          effectLevelValue.value = effectLevelSlider.noUiSlider.get();
-          imgPreview.style.filter = `invert(${effectLevelValue.value}%)`;
-        });
-      }
-
-      if (effectRadioButton.value === 'phobos') {
-        effectLevelSlider.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 3
-          },
-          start: 3,
-          step: 0.1,
-          connect: 'lower',
-        });
-        effectLevelSlider.noUiSlider.on('update', () => {
-          effectLevelValue.value = effectLevelSlider.noUiSlider.get();
-          imgPreview.style.filter = `blur(${effectLevelValue.value}px)`;
-        });
-      }
-
-      if (effectRadioButton.value === 'heat') {
-        effectLevelSlider.noUiSlider.updateOptions({
-          range: {
-            min: 1,
-            max: 3
-          },
-          start: 3,
-          step: 0.1,
-          connect: 'lower',
-        });
-        effectLevelSlider.noUiSlider.on('update', () => {
-          effectLevelValue.value = effectLevelSlider.noUiSlider.get();
-          imgPreview.style.filter = `brightness(${effectLevelValue.value})`;
-        });
-      }
-
-      if (effectRadioButton.value === 'none') {
-        imgPreview.style.removeProperty('filter');
-      }
-
-    });
-  });
+      effectLevelSlider.noUiSlider.updateOptions(effectLevelConfig[currentEffect]);
+      imgPreview.style.filter = `${effectValue[currentEffect]}(${effectLevelSlider.noUiSlider.get()})`;
+    } else {
+      effectLevel.backgroundColor = '';
+      effectLevelSlider.classList.add('hidden');
+      imgPreview.style.removeProperty('filter');
+    }
+  }
 };
+
+effectLevelSlider.noUiSlider.on('update', () => {
+  effectLevelValue.value = effectLevelSlider.noUiSlider.get();
+  imgPreview.style.filter = `${effectValue[currentEffect]}(${effectLevelSlider.noUiSlider.get()})`;
+});
 
 uploadForm.addEventListener('submit', (evt) => {
   if(!pristine.validate()) {
@@ -200,9 +216,17 @@ const closeImageForm = () => {
   editForm.classList.add('hidden');
   document.body.classList.remove('modal-open');
 
+  currentScaleValue = DEFAULT_SCALE_VALUE;
+  imgPreview.style.transform = `scale(${(DEFAULT_SCALE_VALUE) / 100})`;
+
+  imgPreview.className = 'effects__preview--none';
+  effectLevelSlider.classList.add('hidden');
+
   smallerScaleButton.removeEventListener('click', onSmallerScaleButtonClick);
   biggerScaleButton.removeEventListener('click', onBiggerScaleButtonClick);
+  effectList.removeEventListener ('change', onEffectListChange);
 
+  effectLevelSlider.noUiSlider.reset();
   uploadForm.reset();
   pristine.reset();
 };
@@ -221,16 +245,11 @@ const initPostForm = () => {
   uploadFileButton.addEventListener('change', () => {
     editForm.classList.remove('hidden');
     document.body.classList.add('modal-open');
+    imgPreview.style.filter = '';
 
-    scaleControlValue.value = `${DEFAULT_VALUE} %`;
-    imgPreview.style.transform = `scale(${(DEFAULT_VALUE) / 100})`;
-    imgPreview.className = 'effects__preview--none';
-    imgPreview.style.removeProperty('filter');
-    currentValue = DEFAULT_VALUE;
-    effectLevelSlider.classList.add('hidden');
-
-    changeScale();
-    applyEffect();
+    smallerScaleButton.addEventListener('click', onSmallerScaleButtonClick);
+    biggerScaleButton.addEventListener('click', onBiggerScaleButtonClick);
+    effectList.addEventListener ('change', onEffectListChange);
 
     closeFormButton.addEventListener('click', closeImageForm);
     window.addEventListener('keydown', handleKeydown);
